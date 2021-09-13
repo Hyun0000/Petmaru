b1bd373e703eda6f65176359d180168bf72d4515
Hyun0000 <rakpink@naver.com>
2021-09-14 01:10:47 +0900

    21.09.14 - 김현영 - 회원가입 Dao 작성 1단계 : 아이디 중복 체크 - checkId method 2단계 : 회원가입 진행 - joinMember method - 상품 검색 Dao 작성 - searchProduct method
    TODO
    - DBCP 만들기
    - 만든 Dao에 close() method 추가해야 합니다.

--- src/com/pet/rmaru/member/model/dao/MemberDao.java
+++ src/com/pet/rmaru/member/model/dao/MemberDao.java
@@ -1,5 +1,52 @@
 package com.pet.rmaru.member.model.dao;
 
-public class MemberDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.pet.rmaru.member.model.vo.Member;
+
+public class MemberDao {
+	// 회원가입 1단계 : ID 중복체크
+	// result = 1 (기존 아이디 존재, 회원가입 불가) / result = 0 (아이디 없음, 회원가입 가능) 
+	public int checkId(Connection conn, String id) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select * from member where id = ?";
+		// * 보다는 회원번호(시퀀스)를 가져오는 것으로 수정 요망
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				result = 1; // 중복 아이디 존재
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//TODO close() 
+		}
+		return result;
+	}
+	// 회원가입 2단계 : 회원가입 진횅
+	// result = 1 (회원가입 성공) / result = 0 (회원가입 실패) 
+	public int joinMember(Connection conn, Member member) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		String sql = "";
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			//TODO 테이블 작성후 추가로 작성
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//TODO close() 
+		}
+		return result;
+	}
 }
--- src/com/pet/rmaru/product/model/dao/ProductDao.java
+++ src/com/pet/rmaru/product/model/dao/ProductDao.java
@@ -3,11 +3,15 @@ package com.pet.rmaru.product.model.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.pet.rmaru.product.model.vo.Product;
 
 public class ProductDao {
+//==============================================================================================
+//==============================================================================================
+	// 전체 상품 목록 조회
 	public ArrayList<Product> productList(Connection conn, int sRum, int eRun ) {
 		ArrayList<Product> productList = null;
 		Product product = null;
@@ -25,8 +29,33 @@ public class ProductDao {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-				
-		
 		return productList;
+	}
+//==============================================================================================
+//==============================================================================================	
+	// 특정 상품 검색
+	// 검색어(String)를 입력받아 조회
+	public ArrayList searchProduct(Connection conn, String keyword) {
+		ArrayList<Product> arrayList = null;
+		Product product = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		String sql = "select * from product where p_name like %?%";
+		String sql2 = "select * from product where p_name like" + "'%" + keyword + "%'";
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql2);
+			
+			if (rs.next()) {
+				product = new Product();
+				arrayList = new ArrayList<Product>();
+				// TODO product 필드 set 하기 
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return arrayList;
 	}
 }
