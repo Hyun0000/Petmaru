dec9ac93d5e255513de78421a344febed15e6d8b
Hyun0000 <rakpink@naver.com>
2021-10-12 11:47:47 +0900

    21.10.12 김현영 (커밋 테스트)

--- src/com/petmaru/member/write/model/service/WriteMemberReviewService.java
+++ src/com/petmaru/member/write/model/service/WriteMemberReviewService.java
@@ -8,31 +8,31 @@ import com.petmaru.member.write.model.vo.WriteMemberReviewVo;
 
 public class WriteMemberReviewService {
 	//====================================================================================================
-		// 각 상품 카테고리별 후기 조회
-		public ArrayList<WriteMemberReviewVo> productMemberReview(String category, int startRown, int endRown) {
-			ArrayList<WriteMemberReviewVo> productMemberReview = null;
-			Connection conn = getConnection();
-			productMemberReview = new WriteMemberReviewDao().productMemberReview(conn, category, startRown, endRown);
-			close(conn);
-			return productMemberReview;
-		}
-	//====================================================================================================
-		// 각 상품 카테고리 별 후기 글의 총 개수
-		public int allReview(String cateGory) {
-			int result = 0;
-			Connection conn = getConnection();
-			result = new WriteMemberReviewDao().allReview(conn, cateGory);
-			close(conn);
-			return result;
-		}
-	//====================================================================================================
-	//====================================================================================================
-	//====================================================================================================
-	//====================================================================================================
-	//====================================================================================================
-	//====================================================================================================
-	//====================================================================================================
-	//====================================================================================================
+	// 각 상품 카테고리별 후기 조회
+	public ArrayList<WriteMemberReviewVo> productMemberReview(String category, int startRown, int endRown) {
+		ArrayList<WriteMemberReviewVo> productMemberReview = null;
+		Connection conn = getConnection();
+		productMemberReview = new WriteMemberReviewDao().productMemberReview(conn, category, startRown, endRown);
+		close(conn);
+		return productMemberReview;
+	}
+//====================================================================================================
+	// 각 상품 카테고리 별 후기 글의 총 개수
+	public int allReview(String cateGory) {
+		int result = 0;
+		Connection conn = getConnection();
+		result = new WriteMemberReviewDao().allReview(conn, cateGory);
+		close(conn);
+		return result;
+	}
+//====================================================================================================
+//====================================================================================================
+//====================================================================================================
+//====================================================================================================
+//====================================================================================================
+//====================================================================================================
+//====================================================================================================
+//====================================================================================================
 
 
 }
